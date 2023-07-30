30af60cb1ad73266d3c78ee7391467e1d2a3217a
Anjali550 <sainianjali1097@gmail.com>
2023-07-30 12:51:16 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let apiKey = "62a113c0eda80f2a6791271a91b16a3e";
 const inputElement = document.getElementById("myInput");
 
-// inputElement.addEventListener("keydown", function(event) {
-//   if (event.key === "Enter") {
-//     const cityName = event.target.value;
-//     getWeather(cityName);
-//     getDaily(cityName);
-//   }
-// });
+const cityName = inputElement.value;
 function onSearch() {
-  const cityName = inputElement.value;
+  let loader = document.querySelector(".loader");
+  loader.classList.add("visible");
+  let btnn = document.querySelector("#btn1");
+  btnn.style.opacity= 0.1;
+  let btnn2 = document.querySelector("#btn2");
+  btnn2.style.opacity= 0.1;
+  let right1 = document.querySelector("#rightt");
+  right1.style.opacity= 0.1;
+const cityName = inputElement.value;
   getWeather(cityName);
   getDaily(cityName);
+  setTimeout(() => {
+    loader.classList.remove("visible");
+    btnn.style.opacity=1;
+    // btnn.style.display=none;
+    btnn2.style.opacity=1;
+    right1.style.opacity=1;
+  }, 2000);
 }
 
-// let citySearch = (e)=>{
-//   console.log(e.target.innerHTML);
-// }
+// Rest of your code...
+
+// function spin1(){
 
 let getWeather = (cityName) => {
   fetch(
@@ -51,7 +60,14 @@ let getWeather = (cityName) => {
             <div class="cards2">
               <h3>Humidity</h3>
               <p><span id="status">${result.main.humidity}</span>%</p>
-              <input type="range" id="rangeValue" name="rangeValue" min="0" max="100" step="1" value="50">
+              <div class="upper">
+                <p id="rge-text">0</p>
+                <p id="rge-text">50</p>
+                <p id="rge-text">100</p>
+              </div>
+              <div class="progress-bar">
+
+              </div>
             </div>
           </div>
             <div class="card-second">
@@ -74,6 +90,7 @@ let getWeather = (cityName) => {
       console.log(error);
     });
 };
+// getWeather("Delhi");
 
 // for date
 const currentDate = new Date();
@@ -104,24 +121,26 @@ let getDaily = (cityName) => {
   let cardsDiv = document.querySelector(".card");
   cardsDiv.innerHTML = "";
   fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=6&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=6&unit=metric&appid=${apiKey}`
   )
     .then((res) => {
       return res.json();
     })
     .then((result) => {
-      console.log(result.list[1]);
+      // console.log(result.list[1]);
       for (let i = 1; i < result.list.length; i++) {
         cardsDiv.innerHTML += `
           <div class="cards">
           <p>${Date.now()}</p>
-          <img src="LightCloud.png" width="80%"/>
+          <img  src="http://openweathermap.org/img/w/${
+            result.list[i].weather[0].icon
+          }.png" width="80%"/>
          <div class="temp">
-          <p><span class="Rtext">${Math.floor(
-            result.list[i].main.temp_max
+          <p class="p1"><span class="Rtext">${Math.floor(
+            result.list[i].main.temp_max / 10
           )}</span><span> &deg;C</span></p>
-          <p>${Math.floor(
-            result.list[i].main.temp_max
+          <p class="p2">${Math.floor(
+            result.list[i].main.temp_max / 10
           )}</span><span> &deg;C</span></p>
          </div>
         </div>`;
@@ -141,4 +160,39 @@ function searchSection2() {
 function searchSection() {
   element2.style.display = "block";
   element.style.display = "none";
+}
+
+// -------to-change-temp---------
+function getCelcious() {
+  let currentTemp = document.querySelector("#temP");
+  let curr = parseInt(currentTemp.innerText);
+  currentTemp.innerText = ((curr - 32)/10) / 1.8 + "°C";
+  let p1 = document.querySelectorAll(".p1");
+  let p2 = document.querySelectorAll(".p2");
+
+  for (let i = 0; i < p1.length; i++) {
+    let cardP1 = parseInt(p1[i].innerText);
+    p1[i].innerText = ((cardP1 - 32)/10) / 1.8 + "°C";
+  }
+  for (let j = 0; j < p2.length; j++) {
+    let cardP2 = parseInt(p2[j].innerText);
+    p2[j].innerText = ((cardP2 - 32)/10) / 1.8 + "°C";
+  }
+}
+function getFarh() {
+  let currentTemp = document.querySelector("#temP");
+  let curr = parseInt(currentTemp.innerText);
+  currentTemp.innerText = curr * 1.8 + 32 + "°F";
+
+  let p1 = document.querySelectorAll(".p1");
+  let p2 = document.querySelectorAll(".p2");
+
+  for (let i = 0; i < p1.length; i++) {
+    let cardP1 = parseInt(p1[i].innerText);
+    p1[i].innerText = (cardP1 * 1.8)/10 + 32 + "°F";
+  }
+  for (let j = 0; j < p2.length; j++) {
+    let cardP2 = parseInt(p2[j].innerText);
+    p2[j].innerText = (cardP2 * 1.8)/10 + 32 + "°F";
+  }
 }
